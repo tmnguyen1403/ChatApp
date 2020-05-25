db0ca208c84889cb852c69aa67b6edcc02cbd25f
Minh Nguyen <tmnguyen1403@gmail.com>
2020-05-25 14:59:37 -0500

    Handle messages in Message Manager

---
+++ src/MessageManager.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import ChatWindow from './ChatWindow';
+
+class MessageManager extends Component {
+
+	static propTypes = {
+		users: PropTypes.array.isRequired
+	}
+
+	state = {
+		messages: []
+	}
+
+	sendMessage = (message) => {
+		console.log("Receive: ", message);
+		this.setState({
+			messages: [
+				...this.state.messages,
+				message
+			]
+		})
+	}
+
+	render() {
+		const {users} = this.props;
+		return (
+			<React.Fragment>
+				<ChatWindow username={users[0].username} messages={this.state.messages} handleMessage={this.sendMessage} />
+				<ChatWindow username={users[1].username} messages={this.state.messages} handleMessage={this.sendMessage}/>
+			</React.Fragment>
+		);
+	}
+}
+
+export default MessageManager;
